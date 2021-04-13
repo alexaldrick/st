b4a563a605074ee6dffac888bb3c73b66678c482
Alex Aldrick <alex@alexaldrick.com>
2021-04-13 21:17:34 +0200

    font change

--- config.h
+++ config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono Nerd Font:pixelsize=24:antialias=true:autohint=true";
+static char *font = { "Hack:pixelsize=24:antialias=true:autohint=true", "JoyPixels:pixelsize=14:antialias=true:autohint=true"  };
+/* static char *font = "JetBrains Mono Nerd Font:pixelsize=24:antialias=true:autohint=true"; */
 static int borderpx = 2;
 
 
